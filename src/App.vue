5642c593b2471f5984757ec247bf7843ff2609ef
Sergey Doroshenko <SDarashenka@gomel.iba.by>
2021-02-16 07:13:05 +0300

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-jumbotron
+     id="jumbotron">
+      <b-container fluid class="text-center" id="jumbotron-b-container">
+        <b-row align-v="center">
+          <b-col>
+            <h1>Белводпуть</h1>
+          </b-col>
+        </b-row>
+        <b-row align-v="center">
+          <b-col>
+            <b-button variant="primary" href="#">Подробнее</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+#jumbotron {
+  height:100vh;
+  margin: 0;
+}
+#jumbotron-b-container {
+  height: 100vh;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Белводпуть</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+            <b-nav-item active href="#">Услуги</b-nav-item>
+            <b-nav-item active href="#">Контакты</b-nav-item>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
